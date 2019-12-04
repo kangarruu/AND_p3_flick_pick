553a5ad1d0b3e401b20dbfd4fafe3c6eb0934b07
Ru Levin <rulevin@gmail.com>
2019-12-04 14:54:42 -0500

    Add code for instantiating a database sInstance to MovieDatabase.java Add MovieDao method in MovieDatabase.java

--- app/src/main/java/database/MovieDatabase.java
+++ app/src/main/java/database/MovieDatabase.java
@@ -1,10 +1,38 @@
 package database;
 
+import android.content.Context;
+import android.util.Log;
+
+import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.and_p3_flick_pick.model.Movie;
+
+//Database annotation for Room
+@Database(entities = {Movie.class}, version = 1, exportSchema = false)
 public abstract class MovieDatabase extends RoomDatabase {
 
     private static final String LOG_TAG = MovieDatabase.class.getSimpleName();
+    private static final Object LOCK = new Object();
+    private static final String DATABASE_NAME = "flickDatabase";
+    private static MovieDatabase sInstance;
 
+    public static MovieDatabase getInstance(Context context) {
+        //Create a database instance if it does not exist yet
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                Log.d(LOG_TAG, "Creating a new database instance");
+                sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                        MovieDatabase.class,
+                        MovieDatabase.DATABASE_NAME)
+                        .build();
+            }
+        }
+        Log.d(LOG_TAG, "Getting the database instance");
+        return sInstance;
+    }
 
+    //Method for returning the MovieDao for use directly from Database instance
+    public abstract MovieDao movieDao();
 }
