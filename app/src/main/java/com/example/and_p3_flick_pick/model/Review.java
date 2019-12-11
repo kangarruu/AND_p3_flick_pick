287f588a364e21b63737a8f507f5ded14b9f2379
Ru Levin <rulevin@gmail.com>
2019-12-11 14:17:30 -0500

    Add Review class

---
+++ app/src/main/java/com/example/and_p3_flick_pick/model/Review.java
@@ -0,0 +1,78 @@
+package com.example.and_p3_flick_pick.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Review implements Parcelable {
+
+    private int movieId;                       //movie id
+    private String author;                     //author
+    private String content;                    //review text
+    private String url;                        //url to review in TMDB
+
+    public Review(int movieId, String author, String content, String url) {
+        this.movieId = movieId;
+        this.author = author;
+        this.content = content;
+        this.url = url;
+    }
+
+    protected Review(Parcel in) {
+        movieId = in.readInt();
+        author = in.readString();
+        content = in.readString();
+        url = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(movieId);
+        dest.writeString(author);
+        dest.writeString(content);
+        dest.writeString(url);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Review> CREATOR = new Creator<Review>() {
+        @Override
+        public Review createFromParcel(Parcel in) {
+            return new Review(in);
+        }
+
+        @Override
+        public Review[] newArray(int size) {
+            return new Review[size];
+        }
+    };
+
+    //Getters
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "movieId=" + movieId +
+                ", author='" + author + '\'' +
+                ", content='" + content + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
